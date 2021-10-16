0e455828e84616a215e69558b9dc57a2ec5029d0
Gergely Brunda <gergelybrunda@gmail.com>
2021-10-16 21:17:09 +0200

    true and false useing in bool

--- primszam/main.cpp
+++ primszam/main.cpp
@@ -5,11 +5,11 @@ int main() {
 	bool isPrime;
 
 	for (p = 2; p <= 50; p++) {
-		isPrime = 1;
+		isPrime = true;
 		for (d = 2; d < p; d++)
 			if (p % d == 0)
-				isPrime = 0;
-		if (isPrime != 0)
+				isPrime = false;
+		if (isPrime == true)
 			std::cout << p << std::endl;
 	}
 	return 0;
